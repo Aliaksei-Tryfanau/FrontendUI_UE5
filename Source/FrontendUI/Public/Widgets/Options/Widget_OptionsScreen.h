6cc31e7d0299d1ad3be2eeca75be94d74aebfa08
Aliaksei Tryfanau <altrishka@gmail.com>
2025-09-27 19:50:10 -0400

    Tab options widget

---
+++ Source/FrontendUI/Public/Widgets/Options/Widget_OptionsScreen.h
@@ -0,0 +1,52 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Widget_ActivatableBase.h"
+#include "Widget_OptionsScreen.generated.h"
+
+class UOptionsDataRegistry;
+class UFrontendTabListWidgetBase;
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNaiveTick))
+class FRONTENDUI_API UWidget_OptionsScreen : public UWidget_ActivatableBase
+{
+	GENERATED_BODY()
+
+protected:
+	//~ Begin UUserWidget Interface
+	virtual void NativeOnInitialized() override;
+	//~ End UUserWidget Interface
+
+	//~ Begin UCommonActivatableWidget Interface
+	virtual void NativeOnActivated() override;
+	//~ End UCommonActivatableWidget Interface
+
+private:
+	UOptionsDataRegistry* GetOrCreateDataRegistry();
+	
+	void OnResetBoundActionTriggered();
+	
+	void OnBackBoundActionTriggered();
+
+	UFUNCTION()
+	void OnOptionsTabSelected(FName TabId);
+
+	//***** Bound Widgets ***** //
+	UPROPERTY(meta = (BindWidget))
+	UFrontendTabListWidgetBase* TabListWidget_OptionsTabs;
+	//***** Bound Widgets ***** //
+
+	//Handle the creation of data in the options screen. Direct access to this variable is forbidden
+	UPROPERTY(Transient)
+	UOptionsDataRegistry* CreatedOwningDataRegistry;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Frontend Options Screen", meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle ResetAction;
+
+	FUIActionBindingHandle ResetActionHandle;
+};
